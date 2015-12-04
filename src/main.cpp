3a28a902b41dad069f7d652cf8cbf4ee53f72c45
Miroslav Cibulka <xcibul10@stud.fit.vutbr.cz>
2015-12-04 18:45:02 +0100

    Set up workspace, build system, tests

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Miroslav Cibulka
+ * @created 12/4/15
+ * @copyright Copyright (c) 2015 XXX
+ * @detail
+ *
+ */
+
+int main(int argc, const char **argv)
+  {
+    return 0;
+  }
---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file main.cpp
+ * @author Miroslav Cibulka
+ * @created 12/4/15
+ * @copyright Copyright (c) 2015 XXX
+ * @detail
+ *
+ */
+
+#define BOOST_TEST_MODULE "CAS Unit Tests"
+
+#include <boost/test/included/unit_test.hpp>
